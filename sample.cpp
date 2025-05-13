9136f71813fa2aa229eb11abf12d2298680e0880
arjunb2206 <arjun.b@s.amity.edu>
2025-05-13 10:28:10 +0530

    all changes done in sample|sample.cpp

--- sample.cpp
+++ sample.cpp
@@ -2,10 +2,10 @@
 #include<conio.h>
 int main()
 {
-	int a,b,c,sum;
-	cout<<"enter three values";
-	cin>>a>>b>>c;
-	sum=a+b+c;
-	cout<<"sum is"<<sum;
-	getch();
+	int a,b,c,d,sum;
+	cout<<"enter four values";
+	cin>>a>>b>>c>>d;//four values
+	sum=a+b+c+d;//add
+	cout<<"sum is"<<sum;//sum
+	getch();//to get output 
 }
